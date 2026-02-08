7b8de586af75bfb51741dc1756f7ec983baf29e0
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-08 14:07:35 +0530

    Add PatternEscape program

--- src/Lecture1/PatternEscape.java
+++ src/Lecture1/PatternEscape.java
@@ -1,4 +1,9 @@
 package Lecture1;
 
 public class PatternEscape {
+    public static void main(String[] args) {
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *\n");
+        System.out.println("* * * * *\n* * * *\n* * *\n* *\n*\n");
+        System.out.println("        *\n      * *\n    * * *\n  * * * *\n* * * * *");
+    }
 }
