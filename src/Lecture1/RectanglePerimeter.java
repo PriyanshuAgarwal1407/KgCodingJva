3a3571f936c6277e44d7cf087924bf731e08a846
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-08 13:53:59 +0530

    Add RectanglePerimeter program

---
+++ src/Lecture1/RectanglePerimeter.java
@@ -0,0 +1,16 @@
+package Lecture1;
+
+import java.util.Scanner;
+public class RectanglePerimeter {
+    public static void main(String[] args) {
+        // Perimeter of rectangle ABCD = A+B+C+D
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter sides of recatangle: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        System.out.print("Perimeter of rectangle is: " + (a+b+c+d));
+        sc.close();
+    }
+}
