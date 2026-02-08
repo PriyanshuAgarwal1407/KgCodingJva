1f01cbcb5f4233f73e2e403a2317afb48aff8ef0
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-08 13:53:59 +0530

    Add Pattern program

---
+++ src/Lecture1/Pattern.java
@@ -0,0 +1,11 @@
+package Lecture1;
+
+public class Pattern {
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+    }
+}
