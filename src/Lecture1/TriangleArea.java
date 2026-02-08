1be3e4aef9d80ca1e7a38c39e39487cca1da390f
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-08 13:53:59 +0530

    Add TriangleArea program

---
+++ src/Lecture1/TriangleArea.java
@@ -0,0 +1,17 @@
+package Lecture1;
+
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        // Area of triangle = ½*B*H
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter sides of triangle: ");
+        double b = sc.nextDouble();
+        double h = sc.nextDouble();
+
+        double area = (1.0/2.0) * b * h;
+        System.out.println("Area of triangle is: " + area);
+        sc.close();
+    }
+}
