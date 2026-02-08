d082ef995335e3ec66fa738d6b64115488b2d89b
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-08 14:05:44 +0530

    Add VolumeOfCylinder program

--- src/Lecture1/VolumeOfCylinder.java
+++ src/Lecture1/VolumeOfCylinder.java
@@ -1,4 +1,19 @@
 package Lecture1;
 
+import java.util.Scanner;
 public class VolumeOfCylinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius of cylinder: ");
+        double radius = sc.nextDouble();
+
+        System.out.print("Enter height of cylinder: ");
+        double height = sc.nextDouble();
+
+        double volume = Math.PI * Math.pow(radius, 2) * height;
+        System.out.printf("Volume of cylinder is: %.2f", volume);
+
+        sc.close();
+    }
 }
