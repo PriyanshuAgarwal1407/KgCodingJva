46e9c50af661236d9ba28b1f499205693ba426be
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-08 13:53:59 +0530

    Add Input program

---
+++ src/Lecture1/Input.java
@@ -0,0 +1,17 @@
+package Lecture1;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int c = a+b;
+        System.out.print("Addition of " + a + " + " + b + " = " + c);
+        sc.close();
+    }
+}
