2789e95988086d44d4628e1aa66f4e3a65b33d30
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-08 13:53:59 +0530

    Add ArithmeticOperators program

---
+++ src/Lecture1/ArithmeticOperators.java
@@ -0,0 +1,19 @@
+package Lecture1;
+
+import java.util.Scanner;
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        System.out.println("Result of " + a + " + " + b + " = " + (a+b));
+        System.out.println("Result of " + a + " - " + b + " = " + (a-b));
+        System.out.println("Result of " + a + " * " + b + " = " + (a*b));
+        System.out.println("Result of " + a + " / " + b + " = " + (a/b));
+        System.out.println("Result of " + a + " % " + b + " = " + (a%b));
+        sc.close();
+    }
+}
