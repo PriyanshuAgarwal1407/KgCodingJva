8cbc7e5fc823c40a1447baac5814dd067c9f650f
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-12 19:05:40 +0530

    Add ReverseArray: Implement in-place array reversal using two-pointer technique

--- src/Lecture2/Arrays/Questions/ReverseArray.java
+++ src/Lecture2/Arrays/Questions/ReverseArray.java
@@ -1,4 +1,21 @@
 package Lecture2.Arrays.Questions;
 
 public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = ArrayUtility.inputArray();
+        reverse(arr);
+        System.out.println("Your reversed array is:");
+        ArrayUtility.displayArray(arr);
+    }
+
+    public static void reverse(int[] arr){
+        int i = 0, j = arr.length-1;
+        while(i< arr.length/2){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
 }
