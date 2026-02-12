860ce77f9e8eec12fe71420f72fe0345ce420676
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-12 19:05:28 +0530

    Add PrintNegativeElements: Filter and display all negative elements from array

--- src/Lecture2/Arrays/Questions/PrintNegativeElements.java
+++ src/Lecture2/Arrays/Questions/PrintNegativeElements.java
@@ -1,4 +1,22 @@
 package Lecture2.Arrays.Questions;
 
 public class PrintNegativeElements {
+    public static void main(String[] args) {
+        int[] arr = ArrayUtility.inputArray();
+        negativeEle(arr);
+    }
+
+    public static void negativeEle(int[] arr) {
+        boolean found = false;
+        System.out.print("Negative Elements: ");
+        for (int element : arr) {
+            if (element < 0) {
+                System.out.print(element + " ");
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.print("None");
+        }
+    }
 }
