a01525bdd141bd2a26334e92bbc9684dbef94bd3
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-12 19:06:03 +0530

    Add SortArrayBuiltIn: Demonstrate array sorting using Arrays.sort() method

--- src/Lecture2/Arrays/SortArrayBuiltIn.java
+++ src/Lecture2/Arrays/SortArrayBuiltIn.java
@@ -1,4 +1,18 @@
 package Lecture2.Arrays;
 
+import Lecture2.Arrays.Questions.ArrayUtility;
+import java.util.Arrays;
+
 public class SortArrayBuiltIn {
+    public static void main(String[] args) {
+        // sort - ascending order
+        int[] arr = ArrayUtility.inputArray();
+        print(arr);
+        Arrays.sort(arr);
+        print(arr);
+    }
+    public static void print(int[] arr) {
+        System.out.println("Sorted Array is:");
+        ArrayUtility.displayArray(arr);
+    }
 }
