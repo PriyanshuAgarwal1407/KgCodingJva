40effcd0405a93d153877b004760db1091f2acd7
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-12 19:05:17 +0530

    Add MultiplyOddIndex: Multiply elements at odd indices by 2 and add 10 to even indices

--- src/Lecture2/Arrays/Questions/MultiplyOddIndex.java
+++ src/Lecture2/Arrays/Questions/MultiplyOddIndex.java
@@ -1,4 +1,20 @@
 package Lecture2.Arrays.Questions;
 
 public class MultiplyOddIndex {
+    public static void main(String[] args) {
+        int[] arr = ArrayUtility.inputArray();
+        print(arr);
+        for(int i=0; i<arr.length; i++){
+            if(i%2 == 1) arr[i] *= 2;
+            else arr[i] += 10;
+        }
+        print(arr);
+    }
+
+    public static void print(int[] arr) {
+        for(int j : arr) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
 }
