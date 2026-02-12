13d4ca43d5444f9c43b238713d81ec4b9d947c8c
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-12 19:04:41 +0530

    Add BuiltInArrayMethods: Demonstrate Arrays.sort() and Arrays.fill() utility methods

--- src/Lecture2/Arrays/BuiltInArrayMethods.java
+++ src/Lecture2/Arrays/BuiltInArrayMethods.java
@@ -1,4 +1,20 @@
 package Lecture2.Arrays;
 
+import java.util.Arrays;
+
 public class BuiltInArrayMethods {
+    public static void main(String[] args) {
+//        int[] arr = {5,1,7,34,1,87,12};
+//        Arrays.sort(arr);
+//        for(int i=0; i<arr.length; i++){
+//            System.out.print(arr[i]+" ");
+//        }
+//        System.out.println();
+
+        int[] b = new int[5];
+        Arrays.fill(b,18);
+        for(int i=0; i<b.length; i++){
+            System.out.print(b[i]+" ");
+        }
+    }
 }
