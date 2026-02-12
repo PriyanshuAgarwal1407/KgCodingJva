3df06c55af954d02886f653819fafdb075ea3aa8
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-12 19:04:30 +0530

    Add ArrayOfOtherDataTypes: Demonstrate String array creation and iteration

--- src/Lecture2/Arrays/ArrayOfOtherDataTypes.java
+++ src/Lecture2/Arrays/ArrayOfOtherDataTypes.java
@@ -1,4 +1,14 @@
 package Lecture2.Arrays;
 
 public class ArrayOfOtherDataTypes {
+    public static void main(String[] args) {
+//        for(int i=0; i<arr.length; i++){
+//            System.out.print(arr[i]+" ");
+//        }
+
+        String[] arr = {"DSA","Java","DBMS","OOPs","DSD"};
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
