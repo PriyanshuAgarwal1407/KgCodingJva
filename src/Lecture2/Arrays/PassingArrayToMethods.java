126b3789f63e5c2a8e45bae81f8ead5303bb7099
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-12 19:04:52 +0530

    Add PassingArrayToMethods: Demonstrate pass-by-reference behavior with arrays

--- src/Lecture2/Arrays/PassingArrayToMethods.java
+++ src/Lecture2/Arrays/PassingArrayToMethods.java
@@ -1,4 +1,13 @@
 package Lecture2.Arrays;
 
 public class PassingArrayToMethods {
+    public static void main(String[] args) {
+        int[] x = {6,5,1,7};
+        System.out.println(x[0]);
+        change(x);
+        System.out.println(x[0]);
+    }
+    public static void change(int[] y) {
+        y[0] = 20;
+    }
 }
